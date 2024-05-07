5122097da065dc566389b90d6243e1fe4bba23a4
Marcin Rataj <marcinrataj0@gmail.com>
2024-05-07 15:54:33 +0200

    add Generator and SectionWrapper components, start styling, giving props, children, start section in SectionWrapper with elements

---
+++ src/components/Generator.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import SectionWrapper from "./SectionWrapper";
+
+const Generator = () => {
+  return (
+    <SectionWrapper header={"generate your workout"} title=
+    {['It\'s', 'Huge', 'o\'clock']}>
+     asdasdasd
+    </SectionWrapper>
+  )
+};
+
+export default Generator;
